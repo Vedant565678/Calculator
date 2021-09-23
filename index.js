effe395a83935dbc97c160371de10aa85687fd04
Vedant565678 <86054437+Vedant565678@users.noreply.github.com>
2021-09-23 17:07:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const add = document.querySelector("#+")
+const Frist = document.querySelector("#Frist")
+const Second = document.querySelector("#Second")
+
+add.addEventListener("click", ()=>{
+    const FN = Frist.value;
+    const FD = Second.value;
+})
